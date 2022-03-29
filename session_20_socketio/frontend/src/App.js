590477aeafd5a7d8aaa14cd9f32859beea7d6269
Tuananh15062001 <tuananh15062001@gmail.com>
2022-03-29 16:25:23 +0700

    29/03

--- session_20_socketio/frontend/src/App.js
+++ session_20_socketio/frontend/src/App.js
@@ -1,19 +1,106 @@
 import { useState } from "react";
 import "./App.css";
 
+const determineResult = (board) => {
+  // console.log(board)
+  const combinations = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
+  for (let i = 0; i < combinations.length; i++) {
+    const combination = combinations[i];
+    const [cell1, cell2, cell3] = combination;
+    const [x1, y1] = cell1;
+    const [x2, y2] = cell2;
+    const [x3, y3] = cell3;
+
+    if (
+      board[x1][y1] === "X" &&
+      board[x2][y2] === "X" &&
+      board[x3][y3] === "X"
+    ) {
+      return "X";
+    }
+
+    if (
+      board[x1][y1] === "O" &&
+      board[x2][y2] === "O" &&
+      board[x3][y3] === "O"
+    ) {
+      return "O";
+    }
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === "") {
+          return null;
+        }
+      }
+    }
+  }
+  return "draw";
+};
+
 function App() {
-  const [board, setBoard] = useState(
+  const [board, setBoard] = useState([
     ["", "", ""],
     ["", "", ""],
-    ["", "", ""]
-  );
+    ["", "", ""],
+  ]);
+  const [turn, setTurn] = useState("X");
 
   const handleMove = (rowIdx, cellIdx) => {
-    console.log(rowIdx, cellIdx)
-    setBoard(prev => {
-      
-    })
+    // console.log(rowIdx, cellIdx)
+    if (board[rowIdx][cellIdx]) {
+      return;
+    }
+    setBoard((prev) => {
+      prev[rowIdx][cellIdx] = turn;
+      return [...prev];
+    });
+    setTurn((pre) => (pre === "X" ? "O" : "X"));
   };
+
+  const result = determineResult(board);
+  console.log(result);
 
   return (
     <div className="App">
